19798eeffab5edf80d944619f6b3bba3650eaf36
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-04-15 22:59:27 +0000

    Update img element

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -6,22 +6,13 @@ import desktopBgLight from '../images/bg-desktop-light.jpg';
 
 
 const BackgroundImage = ({ darkTheme }) => {
-    const width = window.innerWidth;
 
     return (
         <div style={{width: '100%'}}>
-            <img 
-                style={{display: width > 500 ? 'none' : 'initial'}}
-                src={
-                // width > 500 && !darkTheme ? desktopBgLight : desktopBgDark
-                    width < 500 && !darkTheme ? mobileBgLight : mobileBgDark
-            } className='bg-image' alt="" />
-            <img 
-                style={{display: width < 500 ? 'none' : 'initial'}}
-                src={
-                    width > 500 && !darkTheme ? desktopBgLight : desktopBgDark
-                // width < 500 && !darkTheme ? mobileBgLight : mobileBgDark
-            } className='bg-image' alt="" />
+            <picture>
+                <source srcset={!darkTheme ? mobileBgLight : mobileBgDark} media="(max-width: 650px)" />
+                <img className='bg-image' src={!darkTheme ? desktopBgLight : desktopBgDark} alt="MDN" />
+            </picture>
         </div>
     )
 }
